74132e6d56190e0445e36e66191b042e279d7968
molly mk <mmkover@gmail.com>
2020-08-21 21:00:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word (string)
   array = string.split
   reverse_array = []
-  array.each do |word|
+  array.collect do |word|
     reverse_word = word.reverse
     reverse_array << reverse_word
   end
