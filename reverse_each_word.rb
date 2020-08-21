d68f01106b53e211472d585c49065faa37c36e69
molly mk <mmkover@gmail.com>
2020-08-21 20:21:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word (string)
   array = string.split
+  reverse_array = []
   array.each do |word|
-  
+    reverse_word = word.reverse
+    reverse_array << reverse_word
   end
+  reverse_array.join
 end
