05bb7239188d515b15af92f2ef54064588a13ab0
molly mk <mmkover@gmail.com>
2020-08-21 21:22:08 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+def reverse_each_word (string)
+  array = string.split
+  reverse_array = []
+  array.each do |word|
+    reverse_word = word.reverse
+    reverse_array << reverse_word
+  end
+  reverse_array.join(" ")
+end
+
+def reverse_each_word (string)
+  array = string.split 
+  array.collect{|word| word.reverse}.join(" ")
+  
+end
